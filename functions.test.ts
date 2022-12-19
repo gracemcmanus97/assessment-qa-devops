8b7702bb940cfab428cd153204787dd97ca07d3b
gracemcmanus97 <grace.mcmanus1@outlook.com>
2022-12-18 21:28:04 -0600

    update 3

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,24 @@
 const {shuffleArray} = require('./utils')
 
+// THIS ONE DOES NOT WORK NOT SURE WHY?
+// describe('shuffleArray should', () => {
+//     test('return an array of the same length as the argument sent in', () => {
+//         let array = ['kona', 'hollie', 'banjo']
+//         let result = shuffleArray(array)
+//         expect(result).toBe(3)
+//     })
+//     test('return an array', () => {
+//         let array = ['kona', 'hollie', 'banjo']
+//         let result = shuffleArray(array)
+//         expect(result).toBe('length')
+//     })
+// })
+const myArray = ['kona', 'hollie', 'banjo']
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test(`return array`, () => {
+        expect(Array.isArray(shuffleArray(myArray))).toBe(true)
+    })
+    test('return an array of the same length as the arguemtn sent in', () => {
+        expect(shuffleArray(myArray).length).toBe(myArray.length)
+    })
 })
